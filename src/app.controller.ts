ab7c15ffe5cf685b37def557c8e3f157025f84c5
Afshin <fallahi_a@yahoo.com>
2023-09-04 15:48:26 -0400

    added lambda and dynamodb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import User from './interface';
 
-@Controller()
+@Controller('users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  async getUsers(): Promise<User[]> {
+    return await this.appService.getUsers();
   }
+
+  @Post()
+  async createUser(@Body() user: User): Promise<User> {
+    return await this.appService.createUser(user);
+  }
+
+  // @Get(':id')
+  // async getUser(@Param() id: string): Promise<User> {
+  //   return await this.appService.getUser(id);
+  // }
+
+  // @Delete(':id')
+  // async deleteUser@Param() id: string): Promise<any> {
+  //   return await this.appService.deleteUser(id);
+  // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import * as AWS from "aws-sdk";
+import { v4 as uuid } from "uuid";
+import User from "./interface";
+
+const dynamoDB = process.env.IS_OFFLINE
+  ? new AWS.DynamoDB.DocumentClient({
+      region: "localhost",
+      endpoint: process.env.DYNAMODB_ENDPOINT,
+    })
+  : new AWS.DynamoDB.DocumentClient();
+
+console.log(dynamoDB)
 
 @Injectable()
 export class AppService {
+  async getUsers(): Promise<any> {
+    try {
+      return dynamoDB
+        .scan({
+          TableName: "UsersTable",
+        })
+        .promise();
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+  }
+
+  async createUser(user: User): Promise<any> {
+    const userObj = {
+      id: uuid(),
+      ...user,
+    };
+    try {
+      return await dynamoDB
+        .put({
+          TableName: "UsersTable",
+          Item: userObj,
+        })
+        .promise();
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+  }
+
+  async getUser(id: string): Promise<any> {
+    try {
+      return await dynamoDB
+        .get({
+          TableName:  "UsersTable", //process.env.USERS_TABLE_NAME,
+          Key: { id },
+        })
+        .promise();
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
+  }
+
+  // async deleteUser(id: string): Promise<any> {
+  //   try {
+  //     return await dynamoDB
+  //       .delete({
+  //         TableName: "UsersTable",
+  //         Key: {
+  //           id: id,
+  //         },
+  //       })
+  //       .promise();
+  //   } catch (e) {
+  //     throw new InternalServerErrorException(e);
+  //   }
+  // }
+
   getHello(): string {
     return 'Hello World!';
   }
